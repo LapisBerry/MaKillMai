cb535a4587fb97ef05d515700d1e4a0d92146a22
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-21 00:40:33 +0700

    new BaseCharacter.java

--- src/components/bases/BaseCharacter.java
+++ src/components/bases/BaseCharacter.java
@@ -1,36 +1,103 @@
 package components.bases;
 
-import java.util.ArrayList;
-
 public class BaseCharacter {
-    // Field
+    // Fields
     private int hp;
     private int maxHp;
-    private ArrayList<BaseCard> cards;
+    private int rotPower;
+    private int requiredForPureMagic;
+    private int sumStoneSuppressor;
+    
 
     // Constructor
-    public BaseCharacter(int hp, int maxHp, ArrayList<BaseCard> startingHands) {
-        // TODO: 
+    public BaseCharacter() {
+        setMaxHp(4);
+        setHp(4);
+        setRotPower(0);
+        setRequiredForPureMagic(3);
+        setSumStoneSuppressor(0);
     }
+
+
+    // Dice Methods
+    public void useAttack1(BaseCharacter anotherBaseCharacter) {
+        // TODO
+    }
+
+    public void useAttack2(BaseCharacter anotherBaseCharacter) {
+        // TODO
+    }
+
+    public void useHealthPotion(BaseCharacter baseCharacter) {
+        // TODO
+    }
+
+    public void useRotPower() {/*blank*/}
+
+    public void usePureMagic(int amount) {
+        // TODO
+    }
+
+    public void useStoneSuppressor() {/*blank*/}
+
+
+    // When character rolls and get those dice faces, call these functions, what will happen to them
+    public void rollsIntoAttack1() {/*blank*/}
     
+    public void rollsIntoAttack2() {/*blank*/}
+
+    public void rollsIntoHealthPotion() {/*blank*/}
+
+    public void rollsIntoRotPower() {
+        // TODO
+    }
+
+    public void rollsIntoPureMagic() {/*blank*/}
+
+    public void rollsIntoStoneSuppressor() {
+        // TODO
+    }
+
+
     // Getter Setter
     public int getHp() {
         return hp;
     }
+
     public void setHp(int hp) {
         this.hp = Math.max(0, hp);
         this.hp = Math.min(this.hp, maxHp);
     }
+
     public int getMaxHp() {
         return maxHp;
     }
+
     public void setMaxHp(int maxHp) {
         this.maxHp = maxHp;
     }
-    public ArrayList<BaseCard> getCards() {
-        return cards;
+
+    public int getRotPower() {
+        return rotPower;
     }
-    public void setCards(ArrayList<BaseCard> cards) {
-        this.cards = cards;
+
+    public void setRotPower(int rotPower) {
+        this.rotPower = rotPower;
+    }
+
+    public int getRequiredForPureMagic() {
+        return requiredForPureMagic;
+    }
+
+    public void setRequiredForPureMagic(int requiredForPureMagic) {
+        this.requiredForPureMagic = Math.max(0, requiredForPureMagic);
+    }
+
+    public int getSumStoneSuppressor() {
+        return sumStoneSuppressor;
+    }
+
+    public void setSumStoneSuppressor(int sumStoneSuppressor) {
+        this.sumStoneSuppressor = Math.max(0, sumStoneSuppressor);
     }
 }
