5854a2e299d16ec29e98440c5728ce47a789f8e4
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-21 00:40:50 +0700

    add configs

---
+++ src/config/Configurations.java
@@ -0,0 +1,12 @@
+package config;
+
+public class Configurations {
+    // Character
+    public static final int baseAttackDamage = 1;
+    public static final int baseStartingHealth = 4;
+    public static final int baseStartingRotPower = 0;
+    public static final int baseRequiredForPureMagic = 3;
+
+    // Whole game
+    public static final int rotPool = 9;
+}
---
+++ src/config/enums/DiceFace.java
@@ -0,0 +1,10 @@
+package config.enums;
+
+public enum DiceFace {
+    ATTACK_1,
+    ATTACK_2,
+    HEALTH_POTION,
+    ROT_POWER,
+    PURE_MAGIC,
+    STONE_SUPPRESSOR
+}
