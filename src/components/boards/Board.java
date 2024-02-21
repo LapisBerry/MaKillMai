43581481ccfa8ea86402ddcb522bfee465ef3ead
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-22 01:21:02 +0700

    Create Board.java

---
+++ src/components/boards/Board.java
@@ -0,0 +1,12 @@
+package components.boards;
+
+import java.util.ArrayList;
+
+import components.players.Player;
+
+public class Board {
+    // Fields
+    private ArrayList<Player> circleOfPlayers;
+
+    // TODO
+}
