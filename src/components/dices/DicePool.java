9bcc167dec42fafa46065c5035a37702a451e3cc
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-22 01:18:44 +0700

    Update DicePool.java

--- src/components/dices/DicePool.java
+++ src/components/dices/DicePool.java
@@ -2,7 +2,7 @@ package components.dices;
 
 public class DicePool {
     // Fields
-    private Dice[] dicePool;
+    private Dice[] dicePool; // "Wanna use ArrayList<> instead?" - @LapisBerry
     private boolean[] isDiceLockedAt;
 
     
@@ -14,9 +14,38 @@ public class DicePool {
 
 
     // Methods
-    public void rollAllDices() { // TODO
+    public void rollAllUnlockedDices() {
         for (int i = 0; i < dicePool.length; ++i) {
-
+            if (!isDiceLockedAt[i]) {
+                dicePool[i].rollDice();
+            }
         }
+    }
+
+    public void unlockAllDices() {
+        for (int i = 0; i < isDiceLockedAt.length; ++i) {
+            isDiceLockedAt[i] = false;
+        }
+    }
+
+    public void unlockDiceAt(int index) {
+        if (0 <= index && index < dicePool.length) {
+            isDiceLockedAt[index] = false;
+        }
+    }
+
+    public void lockDiceAt(int index) {
+        if (0 <= index && index < dicePool.length) {
+            isDiceLockedAt[index] = true;
+        }
+    }
+
+    // Getter Setter
+    public Dice[] getDicePool() {
+        return dicePool;
+    }
+
+    public void setDicePool(Dice[] dicePool) {
+        this.dicePool = dicePool;
     }
 }
