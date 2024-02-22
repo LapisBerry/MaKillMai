92d788f1ea64ab8be5d150d8c91fbb611a7a072f
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-22 13:49:30 +0700

    Update Player.java

--- src/components/players/Player.java
+++ src/components/players/Player.java
@@ -1,5 +1,38 @@
 package components.players;
 
+import components.bases.BaseCharacter;
+
 public class Player {
-    // TODO
+    // Fields
+    private String name;
+    private BaseCharacter character;
+    
+
+    // Constructors
+    public Player() {
+        setName("");
+        setCharacter(new BaseCharacter());
+    }
+
+    public Player(String name, BaseCharacter character) {
+        setName(name);
+        setCharacter(character);
+    }
+
+    // Getter Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BaseCharacter getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(BaseCharacter character) {
+        this.character = character;
+    }
 }
