e60bad12e1be877322f6e8fb4cb5e2bfc536c74d
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-02-22 15:33:28 +0700

    start implement Board

--- src/components/boards/Board.java
+++ src/components/boards/Board.java
@@ -8,5 +8,15 @@ public class Board {
     // Fields
     private ArrayList<Player> circleOfPlayers;
 
-    // TODO
+
+    // Constructors
+    public Board() {
+        // TODO: implement constructor
+    }
+
+    // Methods
+    /*
+     * TODO: implement methods
+     * it should be able to tell how far of those people
+     */
 }
