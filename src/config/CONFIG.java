47d4ffd54fee107af62d29348f04a69c17050a03
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-03-06 07:57:45 +0700

    Rename configuration

---
+++ src/config/CONFIG.java
@@ -0,0 +1,24 @@
+package config;
+
+/**
+ * The {@code CONFIG} class
+ * <p>
+ * This class contains only static variable.
+ *
+ * <p>
+ * They should be used in a constructor for default configuration
+ * @author LapisBerry
+ */
+public final class CONFIG {
+    // Character
+    public static final int BASE_ATTACK_DAMAGE = 1;
+    public static final int BASE_STARTING_HEALTH = 4;
+    public static final int BASE_STARTING_ROT_POWER = 0;
+    public static final int BASE_REQUIRED_FOR_PURE_MAGIC = 3;
+
+    // Dice
+    public static final int DICE_POOL_LENGTH = 5;
+    
+    // Whole game
+    public static final int ROT_POOL = 9;
+}
