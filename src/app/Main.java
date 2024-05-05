acd18fcf6e7acb1a1cdd9890c3ffdb8b8cbb6009
LapisBerry <kittiphopkhankaew@gmail.com>
2024-05-06 06:34:29 +0700

    feat: JoinPane gui

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,37 @@
 package app;
 
-public class Main {
+import app.components.JoinPane;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        JoinPane root = new JoinPane();
+        Scene scene = new Scene(root, 1280, 800);
+
+        scene.setOnKeyPressed(e -> {
+            if (Objects.requireNonNull(e.getCode()) == KeyCode.F11) {
+                primaryStage.setFullScreen(!primaryStage.isFullScreen());
+            }
+        });
+
+        primaryStage.fullScreenExitHintProperty().setValue("Press F11 to exit full screen mode.");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // This line will make the username field unfocused when started.
+        root.requestFocus();
     }
 }
---
+++ src/app/components/JoinPane.java
@@ -0,0 +1,68 @@
+package app.components;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class JoinPane extends StackPane {
+    public JoinPane() {
+        super();
+        Title title = new Title("MaKillMai");
+        InputField username = new InputField("username");
+        InputField ipAddress = new InputField("ip-address");
+        JoinButton joinButton = new JoinButton("Join");
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(title, username, ipAddress, joinButton);
+        vbox.setMaxWidth(400);
+        vbox.setMaxHeight(200);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(17);
+
+        getChildren().add(vbox);
+    }
+
+    private static class Title extends Text {
+        private Title(String text) {
+            super(text);
+            setFont(Font.loadFont(ClassLoader.getSystemResource("fonts/Inter-Black.ttf").toString(), 128));
+            setTextAlignment(TextAlignment.CENTER);
+            requestFocus();
+        }
+    }
+
+    private static class InputField extends TextField {
+        private InputField(String promptText) {
+            super();
+            setPromptText(promptText);
+            setFont(Font.loadFont(ClassLoader.getSystemResource("fonts/Inter-ExtraLight.ttf").toString(), 40));
+            setBackground(new Background(new BackgroundFill(Color.valueOf("D9D9D9"), new CornerRadii(10), null)));
+            setMaxWidth(400);
+            setMinHeight(80);
+            setAlignment(Pos.CENTER);
+        }
+    }
+
+    private static class JoinButton extends Button {
+        private JoinButton(String text) {
+            super(text);
+            setFont(Font.loadFont(ClassLoader.getSystemResource("fonts/Inter-SemiBold.ttf").toString(), 40));
+            setBackground(new Background(new BackgroundFill(Color.valueOf("00C2FF"), new CornerRadii(40), null)));
+            setMaxWidth(274);
+            setMinHeight(80);
+            setAlignment(Pos.CENTER);
+            setOnMouseEntered(e -> {
+                setBackground(new Background(new BackgroundFill(Color.valueOf("00A6D1"), new CornerRadii(40), null)));
+                setCursor(javafx.scene.Cursor.HAND);
+            });
+            setOnMouseExited(e -> {
+                setBackground(new Background(new BackgroundFill(Color.valueOf("00C2FF"), new CornerRadii(40), null)));
+            });
+        }
+    }
+}
