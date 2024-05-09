47f0630a1205a1f88c4d697dfd251376b4044fdb
LapisBerry <kittiphopkhankaew@gmail.com>
2024-05-09 20:44:11 +0700

    test: gameTest/componentsTest/playersTest PlayerTest

--- src/game/logic/components/players/Player.java
+++ src/game/logic/components/players/Player.java
@@ -37,7 +37,7 @@ public class Player {
     public void setCharacter(BaseCharacter character) {
         this.character = character;
         // automatically set the owner of the character to this player
-        this.character.setOwner(this);
+        if (character != null) this.character.setOwner(this);
     }
 
     public Role getRole() {
